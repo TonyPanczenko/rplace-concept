15aef3c4a7575b76a8ce6d0b2fb62a4ddc70d8c5
Tony Panczenko <tony.panczenko@gmail.com>
2022-05-22 11:17:49 +0100

    add get-image integration

--- src/components/CanvasContainer.js
+++ src/components/CanvasContainer.js
@@ -1,5 +1,53 @@
+import axios from 'axios';
+import { useEffect, useRef } from 'react';
+
 function CanvasContainer() {
-  
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const imageResX = 100;
+  const imageResY = 100;
+  const imagePxSize = 10;
+  const canvasPxDensity = 2;
+  const canvasPxPerImagePx = imagePxSize * canvasPxDensity;
+
+  useEffect(() => {
+    initCanvas();
+    getImage();
+  }, []);
+
+  const initCanvas = () => {
+    const canvas = canvasRef.current;
+    canvas.width = imageResX * canvasPxPerImagePx;
+    canvas.height = imageResY * canvasPxPerImagePx;
+    const ctx = canvas.getContext('2d');
+    ctx.scale(canvasPxDensity, canvasPxDensity);
+    ctxRef.current = ctx; 
+  };
+
+  const getImage = async () => {
+    const res = await axios.get(`${process.env.API_URL}/get-image`);
+    res.data.pixels.forEach(({ Coordinates, Color }) => {
+      ctxRef.fillStyle = Color;
+      ctxRef.fillRect(
+        Coordinates.x * canvasPxPerImagePx,
+        Coordinates.y * canvasPxPerImagePx,
+        canvasPxPerImagePx,
+        canvasPxPerImagePx
+      );
+    });
+  };
+
+  const selectPixel = ({ offsetX, offsetY }) => {
+    
+  };
+
+  return (
+    <canvas
+      style={{ width: '100%', height: '100%' }}
+      onMouseDown={selectPixel}
+      ref={canvasRef}
+    ></canvas>
+  );
 }
 
 export default CanvasContainer;
