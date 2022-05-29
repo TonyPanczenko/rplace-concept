112de6416cf5871f45407a61bb20ac7696618749
Tony Panczenko <tony.panczenko@gmail.com>
2022-05-29 15:48:06 +0100

    format date in tooltip

--- src/components/PixelTooltip.js
+++ src/components/PixelTooltip.js
@@ -22,13 +22,22 @@ function PixelTooltip({ pixel, canvasRef }) {
     x: windowCoords.x + offsetXPx,
     y: windowCoords.y + offsetYPx
   };
+  const pixelAddedDate = new Date(timestamp);
+  const printableDate = new Intl.DateTimeFormat(navigator.language, {
+    month: 'short',
+    day: 'numeric'
+  }).format(pixelAddedDate);
+  const printableTime = new Intl.DateTimeFormat(navigator.language, {
+    hour: 'numeric',
+    minute: 'numeric'
+  }).format(pixelAddedDate);
 
   return (
     <div 
       className={styles.tooltip}
       style={{left: `${tooltipCoords.x}px`, top: `${tooltipCoords.y}px`}}
     >
-      <p>Placed by {userIp} at {timestamp}</p>
+      <p>Placed by [{userIp}] at {printableTime} on {printableDate}</p>
     </div>
   );
 }
